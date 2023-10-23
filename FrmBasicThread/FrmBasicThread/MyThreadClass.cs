3ed81e4cfc102282efaa949fd7993f442665c716
JeffersonAlot <144678321+JeffersonAlot@users.noreply.github.com>
2023-10-23 17:35:16 +0800

    Add files via upload

---
+++ FrmBasicThread/FrmBasicThread/MyThreadClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrmBasicThread
+{
+    internal class MyThreadClass
+    {
+        public static Thread Thread1;
+        public static Thread Thread2;
+        public static void Thread1Loop()
+        {
+            for (int loopCount1 = 0; loopCount1 <= 2; loopCount1++)
+            {
+                Thread1 = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + Thread1.Name + " = " + loopCount1);
+                Thread.Sleep(500);
+            }
+            
+        }
+
+        public static void Thread2Loop()
+        {
+            for (int loopCount2 = 0; loopCount2 <= 6; loopCount2++)
+            {
+                Thread2 = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + Thread2.Name + " = " + loopCount2);
+                Thread.Sleep(1500);
+            }
+        }
+        
+    }
+}
---
+++ FrmBasicThread/FrmBasicThread/frmTrackThread.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FrmBasicThread
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string start = "- Before starting Thread -";
+            label1.Text = start;
+            Console.WriteLine(start);
+
+            Thread ThreadA, ThreadB, ThreadC, ThreadD;
+
+            ThreadStart delThreadStart1 = new ThreadStart(MyThreadClass.Thread1Loop);
+
+            ThreadStart delThreadStart2 = new ThreadStart(MyThreadClass.Thread2Loop);
+
+            ThreadA = new Thread(delThreadStart1);
+            ThreadA.Name = "Thread A Process";
+            ThreadB = new Thread(delThreadStart2);
+            ThreadB.Name = "Thread B Process";
+            ThreadC = new Thread(delThreadStart1);
+            ThreadC.Name = "Thread C Process";
+            ThreadD = new Thread(delThreadStart2);
+            ThreadD.Name = "Thread D Process";
+
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Normal;
+            ThreadC.Priority = ThreadPriority.AboveNormal;
+            ThreadD.Priority = ThreadPriority.BelowNormal;
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+
+            string end = "- End of Thread -";
+            label1.Text = end;
+            Console.WriteLine(end);
+        }
+    }
+}
